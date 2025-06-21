83590b3cae9b1ae25dd40f2fd65cf12fde5fc45e
ishitapasalkar_101 <ishitapasalkar101@gmail.com>
2025-06-21 13:42:27 +0530

    Create BASICS.js

---
+++ DAY_6 ASSIGNMENT/BASICS.js
@@ -0,0 +1,16 @@
+
+var myName = "ISHITA";
+let myAge = 20;
+const isStudent = false;
+
+let colors = ["yellow", "green", "blue"];
+let person = {
+  name: "ISHITA",
+  age: 20
+};
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+console.log(colors);
+console.log(person);
