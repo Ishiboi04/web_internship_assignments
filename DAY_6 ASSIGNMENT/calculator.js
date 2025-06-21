0b51354c379937405c00783ebc9a0583837a82dc
ishitapasalkar_101 <ishitapasalkar101@gmail.com>
2025-06-21 13:44:15 +0530

    Create calculator.js

---
+++ DAY_6 ASSIGNMENT/calculator.js
@@ -0,0 +1,9 @@
+
+
+let number1 = parseFloat(prompt("Enter first number:"));
+let number2 = parseFloat(prompt("Enter second number:"));
+
+console.log("Add:", number1 + number2);
+console.log("Subtract:", number1 - number2);
+console.log("Multiply:", number1 * number2);
+console.log("Divide:", number1 / number2);
