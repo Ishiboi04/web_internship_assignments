61143bae5f153b2faeb7b199cd1a8fedbeb701d1
ishitapasalkar_101 <ishitapasalkar101@gmail.com>
2025-06-21 13:50:18 +0530

    Create function.js

---
+++ DAY_6 ASSIGNMENT/function.js
@@ -0,0 +1,11 @@
+function greetUser(name) {
+  console.log("Hello, " + name + "!");
+}
+
+function square(number) {
+  return number * number;
+}
+
+
+greetUser("ISHITA");
+console.log("Square of 4 is", square(4));
